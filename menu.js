3c8a6fa5b84ca4acdc7956e8a4c91414a3964a92
LourdesMp <lourdes.merino@imax.com.pe>
2020-12-11 18:15:04 -0500

    editando funciones jquery

--- menu.js
+++ menu.js
@@ -1,7 +1,27 @@
-$(".submenu").click(function(){
-    $(this).children("ul").slideToggle();
-})
+$(document).ready(main);
 
-$("ul").click(function(p){
-    p.stopPropagation();
-})
+var contador = 1;
+
+function main () {
+    $('.menu-bar').click(function(){
+        if(contador == 1){
+            $('nav').animate({
+                left:'0'
+            });
+            contador = 0;
+        } else{
+            contador = 1;
+            $('nav').animate({
+                left:'-100%'
+            })
+        }
+    })
+
+    // Mostramos y ocultamos submenus
+    $('.submenu').click(function(){
+        $(this).children('.children').slidetoggle();
+    })
+
+
+
+}
