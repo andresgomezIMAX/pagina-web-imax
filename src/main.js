06fed50e6830c591f1b953a93a93b7069dcb21eb
LourdesMp <lourdes.merino@imax.com.pe>
2020-12-13 23:36:47 -0500

    funcion init

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { changeView } from './views-controler/router.js'
+
+const init = () =>{
+    changeView(window.location.hash)
+    window.addEventListener('hashchange', ()=> changeView(window.location.hash))
+
+}
+
+
+window.addEventListener('load', init)
