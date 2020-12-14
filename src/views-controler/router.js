47b56489ae3bc5501ea6545b923a89cda3e20b71
LourdesMp <lourdes.merino@imax.com.pe>
2020-12-13 23:37:39 -0500

    agregando router de SPA

---
+++ src/views-controler/router.js
@@ -0,0 +1,23 @@
+import { components } from '../views/index.js';
+
+const changeView = (hash) => {
+    const id = hash.split('/')[1];
+    const sectionMain = document.getElementById('container');
+    sectionMain.innerHTML = '';
+   switch (hash) {
+       case '': 
+       case '#':
+       case '#/':
+            { return sectionMain.appendChild(components.home()); }
+ 
+       case '#/nosotros': 
+            { return sectionMain.appendChild(components[id]()); }
+ 
+   
+       default:
+        { return sectionMain.appendChild(components.different()); }
+   }
+   console.log(hash)
+}
+
+export { changeView}
