e9bd05e8d66582ec056d8e231b3beda847bb33a6
LourdesMp <lourdes.merino@imax.com.pe>
2020-12-13 23:38:02 -0500

    llamando las vistas de SPA

---
+++ src/views/index.js
@@ -0,0 +1,11 @@
+import Home from './home.js';
+import Nosotros from './nosotros.js';
+import Different from './different.js';
+
+const components = {
+    home: Home,
+    nosotros: Nosotros,
+    different: Different
+}
+
+export {components}
