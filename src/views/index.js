e0edc9fef1e11c7dc4430828b4559c4980d65481
LourdesMp <lourdes.merino@imax.com.pe>
2020-12-14 23:14:44 -0500

    agregando vistas para SPA

--- src/views/index.js
+++ src/views/index.js
@@ -1,11 +1,17 @@
 import Home from './home.js';
 import Nosotros from './nosotros.js';
+import Staff from './staff.js';
+import Servicios from './servicios.js'
 import Different from './different.js';
+
 
 const components = {
     home: Home,
     nosotros: Nosotros,
+    staff: Staff,
+    servicios: Servicios,
     different: Different
+    
 }
 
 export {components}
