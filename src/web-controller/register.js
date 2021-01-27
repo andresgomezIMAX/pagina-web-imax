b2bff57633c97937122e6e0640fa4f71b2431989
LourdesMp <lourdes.merino@imax.com.pe>
2021-01-27 14:18:29 -0500

    guardando en firebase la firma electronica

--- src/web-controller/register.js
+++ src/web-controller/register.js
@@ -1,7 +1,36 @@
-const register = document.querySelector('.box-fill-data');
-const user = () => firebase.auth().currentUser;
 
-// register con email
+//CAPTURANDO EL URL DE LA FIRMA ELECTRONICA
+const currentUser = () => firebase.auth().currentUser;
+const firmRegister = document.querySelector('.firm-register');
+  console.log(firmRegister)
+  if(firmRegister){
+    firmRegister.addEventListener('change', (e) => {
+      console.log('CLICK SUBIR IMAGEN', e.target.files[0]);
+      // Get file
+      file = e.target.files[0];
+      if(file){
+        const storageRef = firebase.storage().ref(`firmRegister/${currentUser().email}/${file.name}`);
+        const task = storageRef .put(file);
+        console.log(task)
+        let urlfirmRegister = '';
+        // Update progress bar
+          task.on('state_changed', () => {
+            task.snapshot.ref.getDownloadURL()
+              .then((url) => {
+                console.log('File available at', url);
+                urlfirmRegister = url;
+                console.log('holaaaaaaaaaaaa', urlfirmRegister);
+                sessionStorage.setItem('firmRegister', urlfirmRegister);
+              })
+             
+          });
+       
+    }
+})
+}
+
+// REGISTRANDO USUARIOS 
+const register = document.querySelector('.box-fill-data');
 register.addEventListener('submit', (e) => {
   e.preventDefault();
   const name = document.querySelector('.name-register').value;
@@ -14,10 +43,9 @@ register.addEventListener('submit', (e) => {
   const leader = document.querySelector('.leader-register').value;
   const entryDay = document.querySelector('.fechaAdmin').value;
   const salarioAdmin= document.querySelector('.salarioAdmin-register').value;
-  const addImg = document.querySelector('.firm-register').value;
+  const urlfirmRegister = sessionStorage.getItem('firmRegister');
+  console.log(name, checkAdmin, dni, phone, email, password, area,leader,entryDay,salarioAdmin,urlfirmRegister)
   
-
-  console.log(name, checkAdmin, dni, phone, email, password, area,leader,entryDay,salarioAdmin,addImg)
   firebase.auth().createUserWithEmailAndPassword(email,  password)
   .then(userCredential => {
     const db = firebase.firestore();
@@ -32,7 +60,7 @@ register.addEventListener('submit', (e) => {
         leader,
         entryDay,
         salarioAdmin,
-        addImg
+        urlfirmRegister
       });
      
   }).then(() => {
@@ -50,6 +78,64 @@ register.addEventListener('submit', (e) => {
   });
 
 });
+
+// PARA MOSTRAR DATOS EN LA TABLA DE BOLETAS
+const onGetUsers = (callback) => firebase.firestore().collection('users').onSnapshot(callback);
+const getUsers = () => firebase.firestore().collection('users').get();
+// const deletePost = id => firebase.firestore().collection('users').doc(id).delete();
+
+const userContainer = document.querySelector('.table-users')
+window.addEventListener('DOMContentLoaded', async(e) => {
+  onGetUsers((querySnapshot)=>{
+    userContainer.innerHTML='';
+    querySnapshot.forEach(doc => {
+      const user = doc.data();
+      console.log(user)
+      user.id = doc.id;
+      const users = firebase.auth().currentUser;
+      userContainer.innerHTML +=  `
+                                  <tr>
+                                  <td> ${user.name}</td>  
+                                  <td>${user.dni}</td>
+                                  <td>${user.email}</td>  
+                                  <td>${user.password}</td>
+                                  <td>${user.area}</td>  
+                                  <td>${user.leader}</td>
+                                  <td>${user.entryDay}</td>  
+                                  <td>${user.salarioAdmin}</td>
+                                  <td><a href=${user.urlfirmRegister} download="Boleta.pdf"><button><i class="fas fa-download"></i>Descargar</button></a></td>
+                                  <td>${user.checkAdmin}</td>
+                                  <td><i class="fas fa-edit"></i> <i class="fas fa-trash-alt"></i></td>
+                                </tr>  
+                             `;
+
+
+                  // const btnsRemove = document.querySelectorAll('.btnRemove');
+                  // btnsRemove.forEach(btn => {
+                  //   btn.addEventListener('click', async (e) => {
+                 
+                  //    await deletePost(e.target.dataset.id)
+                  //   })
+                  // });
+
+                  // const btnsEdit = document.querySelectorAll('.btnEdit');
+                  // btnsEdit.forEach((btn) => {
+                  //   btn.addEventListener('click', async(e) => {
+                  //    const doc = await getPostEdit(e.target.dataset.id)
+                  //    console.log(doc.data())
+                  //    const post = doc.data();
+                  //    editStatus = true;
+                  //    id = doc.id;
+                  //    const inputTextArea = document.querySelector ('.textarea');
+                  //    inputTextArea.value = post.content;
+                  //    btnNewPost.innerHTML = 'Actualizar'
+                  //   })
+                  // });
+    });
+  })
+ 
+})
+
 
 //funcion para mostrar lista de colaboradores en workerAdmin
 const btnlista = document.querySelector('.list-worker');
