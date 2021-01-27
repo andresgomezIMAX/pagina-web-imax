e52641861473f9568f5043cf248fe0c6da234d3a
LourdesMp <lourdes.merino@imax.com.pe>
2021-01-26 23:59:21 -0500

    capturando datos del formulario y creando coleccion de datos Vacaciones

---
+++ src/web-controller/vacation.js
@@ -0,0 +1,35 @@
+const registerVacation = document.querySelector('.box-check-boss');
+const user = () => firebase.auth().currentUser;
+
+registerVacation.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userLogueado = firebase.auth().currentUser;
+    console.log(userLogueado)
+    const useruid = userLogueado.uid;
+    console.log(useruid)
+    const inicioVacation = document.querySelector('.inicio-Vacation').value;
+    const finVacation = document.querySelector('.fin-Vacation').value;
+    const bossInmediate = document.querySelector('.boss-inmediate').value;
+  
+    console.log(useruid,inicioVacation,finVacation,bossInmediate)
+    if(inicioVacation,finVacation){
+        saveVacation(useruid,inicioVacation,finVacation,bossInmediate).then(() => {
+            // sessionStorage.removeItem('fileNewTicked');
+            console.log('se registró solicitud de vacaciones');
+            registerVacation.reset();
+            alert('Se registró solicitud de vacaciones');
+        });
+    }
+    
+  
+  });
+
+  const  saveVacation = (useruid,inicioVacation,finVacation,bossInmediate) => {
+    const firestore = firebase.firestore();
+    return firestore.collection('vacation').add({
+        useruid,
+        inicioVacation,
+        finVacation,
+        bossInmediate
+    });
+};
