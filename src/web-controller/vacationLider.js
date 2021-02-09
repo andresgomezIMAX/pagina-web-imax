afe592b29c739b1a7645664077857012dbfabc3f
LourdesMp <lourdes.merino@imax.com.pe>
2021-02-09 07:12:27 -0500

    Âagregando funcion checkbox

--- src/web-controller/vacationLider.js
+++ src/web-controller/vacationLider.js
@@ -31,31 +31,83 @@ window.addEventListener('DOMContentLoaded', async(e) => {
                                         <td>${vacation.startOfVacation} al ${vacation.endOfVacation}</td>
                                         <td>Pendientes: ${vacation.vacationPending} días <br>
                                             Vencen: ${(vacation.resDateExpireYear) ? vacation.resDateExpireYear : vacation.resDateExpireYear2} </td>
-                                        <td><input type="checkbox" class="conformidad" value=false name="conformidad"></td>
+                                        <td><input type="checkbox" class="conformidad" value=false name="conformidad" data-id="${vacation.id}"></td>
                                         </tr> 
                              `;
-      const checkbox = document.querySelector('.conformidad');
-      console.log(checkbox)
-      checkbox.addEventListener( 'change', function() {
-          if(this.checked) {
-            firebase.firestore().collection('vacation').onSnapshot((querySnapshot) => {
-              querySnapshot.forEach((doc) => {
-                  console.log(`${doc.id} => ${doc.data().useruid}`);
-                  const userLogueado = firebase.auth().currentUser;
-                  const useruid = userLogueado.uid;
-                  if(useruid === doc.data().useruid){
-                      const cityRef = firebase.firestore().collection('vacation').doc(doc.id);
-                      const res = cityRef.update({
-                        confirmacion : true
-                      }, { merge: true });
-                  }
+     
+                        
 
-              })
-          })
-          } 
-      });
 
     });
+
+    const checkboxs = document.querySelectorAll('.conformidad');
+    checkboxs.forEach(check => {
+     check.addEventListener( 'change', function(e) {
+       if(this.checked) {
+         querySnapshot.forEach(doc => {
+           console.log(`${doc.id}`);
+           console.log(e.target.dataset.id)
+           if( e.target.dataset.id === doc.id){
+             console.log('joalaaa')
+            
+             const cityRef = firebase.firestore().collection('vacation').doc(doc.id);
+             const res = cityRef.update({
+               confirmacion : true
+             }, { merge: true });
+         }
+         //    firebase.firestore().collection('vacation').onSnapshot((querySnapshot) => {
+         //    querySnapshot.forEach(doc => {
+         //      const vacation = doc.data();
+         //      console.log(`${doc.id} => ${vacation.id}`);
+
+         //      console.log(vacation.id)
+
+
+         //      if( vacation.id === doc.id){
+         //          const cityRef = firebase.firestore().collection('vacation').doc(doc.id);
+         //          const res = cityRef.update({
+         //            confirmacion : true
+         //          }, { merge: true });
+         //      }
+         //    })
+         //  })
+         })
+        
+      } else {
+       querySnapshot.forEach(doc => {
+         console.log(`${doc.id}`);
+         console.log(e.target.dataset.id)
+         if( e.target.dataset.id === doc.id){
+           console.log('joalaaa')
+          
+           const cityRef = firebase.firestore().collection('vacation').doc(doc.id);
+           const res = cityRef.update({
+             confirmacion : false
+           }, { merge: true });
+       }
+     })
+      }
+      //else {
+      //    firebase.firestore().collection('vacation').onSnapshot((querySnapshot) => {
+      //      querySnapshot.forEach((doc) => {
+      //          console.log(`${doc.id} => ${doc.data().useruid}`);
+      //          const userLogueado = firebase.auth().currentUser;
+      //          const useruid = userLogueado.uid;
+      //          if(useruid === doc.data().useruid){
+      //              const cityRef = firebase.firestore().collection('vacation').doc(doc.id);
+      //              const res = cityRef.update({
+      //                confirmacion : false
+      //              }, { merge: true });
+      //          }
+
+      //      })
+      //  })
+      //  }
+      });
+    })
+ 
+
+
   })
  
 });
