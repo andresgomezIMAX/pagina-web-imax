a7119ecaf8328507d63913138e2fe74e9bc466e0
LourdesMp <lourdes.merino@imax.com.pe>
2021-02-12 17:04:10 -0500

    funcion para editar pago
    agregando total de pago en la tabla

--- src/web-controller/boletasAdmin.js
+++ src/web-controller/boletasAdmin.js
@@ -1,102 +1,85 @@
-let editStatus = false;
-let id = '';
-
 //Para colaboradores el jefe inmediato por defecto
 const boxNameWorker = document.querySelector('.nameWorker');
 console.log(boxNameWorker)
 boxNameWorker.innerHTML = '',
-firebase.firestore().collection('users').onSnapshot((querySnapshot) => {
+  firebase.firestore().collection('users').onSnapshot((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-        // console.log(`${doc.id} => ${doc.data().leader}`);
-        const userLogueado = firebase.auth().currentUser;
-        const useruid = userLogueado.uid;
-            boxNameWorker.innerHTML += `
+      // console.log(`${doc.id} => ${doc.data().leader}`);
+      const userLogueado = firebase.auth().currentUser;
+      const useruid = userLogueado.uid;
+      boxNameWorker.innerHTML += `
                 <option value="${doc.data().name}">${doc.data().name}</option>`
-      
-       
-       
+
+
+
     })
-})
+  })
 
 
 // GUARDANDO URL DEL PDF DE LA BOLETA
 let file;
 const currentUser = () => firebase.auth().currentUser;
 const addTicked = document.querySelector('.addTicked')
-  if(addTicked){
-    addTicked.addEventListener('change', (e) => {
-      console.log('CLICK SUBIR IMAGEN', e.target.files[0]);
-      // Get file
-      let file = e.target.files[0];
-      if(file){
-        const storageRef = firebase.storage().ref(`tickedFile/${currentUser().email}/${file.name}`);
-        const task = storageRef .put(file);
-        console.log(task)
-        let urlBoleta = '';
-        // Update progress bar
-          task.on('state_changed', () => {
-            task.snapshot.ref.getDownloadURL()
-              .then((url) => {
-                console.log('File available at', url);
-                urlBoleta = url;
-                console.log('holaaaaaaaaaaaa', urlBoleta);
-                sessionStorage.setItem('fileNewTicked', urlBoleta);
-              })
-             
-          });
-       
+if (addTicked) {
+  addTicked.addEventListener('change', (e) => {
+    console.log('CLICK SUBIR IMAGEN', e.target.files[0]);
+    // Get file
+    let file = e.target.files[0];
+    if (file) {
+      const storageRef = firebase.storage().ref(`tickedFile/${currentUser().email}/${file.name}`);
+      const task = storageRef.put(file);
+      console.log(task)
+      let urlBoleta = '';
+      // Update progress bar
+      task.on('state_changed', () => {
+        task.snapshot.ref.getDownloadURL()
+          .then((url) => {
+            console.log('File available at', url);
+            urlBoleta = url;
+            console.log('holaaaaaaaaaaaa', urlBoleta);
+            sessionStorage.setItem('fileNewTicked', urlBoleta);
+          })
+
+      });
+
     }
-})
+  })
 }
 
 // ADMINISTRADOR GENERAR BOLETA
 const btnGenerarBoleta = document.querySelector('.btn-generar-boleta');
 const generarBoleta = document.querySelector('.generate-ticket');
 btnGenerarBoleta.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log('hola');
-    // const userLogueado = firebase.auth().currentUser;
-    // console.log(userLogueado)
-    // const useruid = userLogueado.uid;
-    const nameWorker = document.querySelector('.nameWorker').value;
-    const month = document.querySelector('.month').value; 
-    const totalPage = document.querySelector('.totalPage').value;
-    const urlBoleta = sessionStorage.getItem('fileNewTicked');
-    console.log(nameWorker,month,totalPage, urlBoleta)
-    if(urlBoleta){
-      if(!editStatus){
-        saveBoleta(nameWorker, month, totalPage, urlBoleta).then(() => {
-          // sessionStorage.removeItem('fileNewTicked');
-          console.log('se registró boleta');
-          generarBoleta.reset();
-          alert('se registró boleta');
-        });
-      } else{
-         updatePage(id, {
-          nameWorker, 
-          month, 
-          totalPage,
-          urlBoleta
-         })
-      }
-      editStatus = false;
-      id = '';
-      btnGenerarBoleta.innerHTML = 'Publicar'
-  
-      
-    }
-    
+  e.preventDefault();
+  console.log('hola');
+  // const userLogueado = firebase.auth().currentUser;
+  // console.log(userLogueado)
+  // const useruid = userLogueado.uid;
+  const nameWorker = document.querySelector('.nameWorker').value;
+  const month = document.querySelector('.month').value;
+  const totalPage = document.querySelector('.totalPage').value;
+  const urlBoleta = sessionStorage.getItem('fileNewTicked');
+  console.log(nameWorker, month, totalPage, urlBoleta)
+  if (urlBoleta) {
+    saveBoleta(nameWorker, month, totalPage, urlBoleta).then(() => {
+      // sessionStorage.removeItem('fileNewTicked');
+      console.log('se registró boleta');
+      generarBoleta.reset();
+      alert('se registró boleta');
+    });
+  }
+
 });
 
 //FUNCIÓN DE FIREBASE PARA CREAR LA COLECCION DE BOLETAS 
 const saveBoleta = (nameWorker, month, totalPage, urlBoleta) => {
-    const firestore = firebase.firestore();
-    return firestore.collection('pages').add({
-        nameWorker,
-        month,
-        totalPage,
-        urlBoleta,
-    });
+  const firestore = firebase.firestore();
+  return firestore.collection('pages').add({
+    nameWorker,
+    month,
+    totalPage,
+    urlBoleta,
+  });
 };
 
 //PARA MOSTRAR LAS BOLETAS GUARDADAS EN LA TABLA DE 'PAGES'
@@ -104,79 +87,136 @@ const onGetPages = (callback) => firebase.firestore().collection('pages').onSnap
 const getPagesEdit = (id) => firebase.firestore().collection('pages').doc(id).get();
 const getUsers = () => firebase.firestore().collection('users').get();
 const deletePost = id => firebase.firestore().collection('pages').doc(id).delete();
-const updatePage = (id, contentPage) => firebase.firestore().collection('posts').doc(id).update(contentPage);
+const editWorker = (id, nameWorker, month, ) => firebase.firestore().collection('posts').doc(id).update({
+  nameWorker,
+  month
+});
 
 const pageContainer = document.querySelector('.table-page')
-window.addEventListener('DOMContentLoaded', async(e) => {
-  onGetPages((querySnapshot)=>{
-    pageContainer.innerHTML='';
+window.addEventListener('DOMContentLoaded', async (e) => {
+  onGetPages((querySnapshot) => {
+    pageContainer.innerHTML = '';
     querySnapshot.forEach(doc => {
       const page = doc.data();
       console.log(page)
       page.id = doc.id;
       const user = firebase.auth().currentUser;
-      pageContainer.innerHTML +=  `
-                              <tr>
-                                <td> ${page.nameWorker}</td>  
-                                <td> ${page.month}</td>
-                                <td><a href=${page.urlBoleta} download="Boleta.pdf"><button><i class="fas fa-download"></i> Descargar</button></a></td>
-                                <td><input type="checkbox" name="fieldName" value="Check Value" readonly="readonly" onclick="javascript: return false;"/></td>
-                                <td><i data-id="${page.id}" class="btnEdit fas fa-edit"></i> <i class="deletePage fas fa-trash-alt" data-id="${page.id}"></i></td>
+      pageContainer.innerHTML += `
+                              <tr class="row-pages" contenteditable="false" data-id="${page.id}">
+                                <td data-id="${page.id}" class="nameWorkerPage"> ${page.nameWorker}</td>  
+                                <td data-id="${page.id}" class="monthPage"> ${page.month}</td>
+                                <td data-id="${page.id}" class="totalPage"> ${page.totalPage}</td>
+                                <td data-id="${page.id}" class="urlPage"> <a href=${page.urlBoleta} download="Boleta.pdf"><button><i class="fas fa-download"></i> Descargar</button></a></td>
+                                <td data-id="${page.id}" class="checkPage"> <input type="checkbox" name="fieldName" value="Check Value" readonly="readonly" onclick="javascript: return false;"/></td>
+                                <td><i data-id="${page.id}" class="btnEdit fas fa-edit"></i> <button data-id="${page.id}" class="btnSaveWorker" id="btnSaveWorker">💾</button> <i class="deletePage fas fa-trash-alt" data-id="${page.id}"></i></td>
                               </tr>
                              `;
 
 
-                  const deletePage = document.querySelectorAll('.deletePage');
-                  deletePage.forEach(btn => {
-                    btn.addEventListener('click', async (e) => {
-                      confirm('¿Quieres eliminar esta Boleta de Pago?')
-                     await deletePost(e.target.dataset.id)
-                    })
-                  });
+      const deletePage = document.querySelectorAll('.deletePage');
+      deletePage.forEach(btn => {
+        btn.addEventListener('click', async (e) => {
+          const res = confirm('¿Quieres eliminar esta Boleta de Pago?')
+          if (res === true){
+            await deletePost(e.target.dataset.id)
+          }
+        })
+      });
 
-                  const btnsEdit = document.querySelectorAll('.btnEdit');
-                  btnsEdit.forEach(btn => {
-                    btn.addEventListener('click', async(e) => {
-                     const doc = await getPagesEdit(e.target.dataset.id)
-                     console.log(doc.data())
-                     const page = doc.data();
-                     editStatus = true;
-                     id = doc.id;
-                     const nameWorker = document.querySelector('.nameWorker');
-                     nameWorker.value = page.nameWorker;
-                     const month = document.querySelector('.month'); 
-                     month.value = page.month;
-                     const totalPage = document.querySelector('.totalPage');
-                     totalPage.value = page.totalPage;
-                     const urlBoleta = '';
-                     urlBoleta.value = page.urlBoleta;
-                    //  const inputTextArea = document.querySelector ('.textarea');
-                    //  inputTextArea.value = post.content;
-                    btnGenerarBoleta.innerHTML = 'Actualizar'
-                    })
-                  });
+      const btnEditWorker = document.querySelectorAll('.btnEdit');
+      const btnUpdateWorker = document.querySelectorAll('.btnSaveWorker');
+      const editableWorker = document.querySelectorAll('.row-pages');
 
-                    const filterPageWorker = (data, texto) => {
-                    const longNameWorker = texto.length;
-                    // const dataName =  fs.collection('users').get().then((snapshot) => {console.log(snapshot.docs);
-                    //     // setupPosts(snapshot.docs)
-                    // });
-                    const dataName = page.nameWorker;
-                    const filterName = dataName.filter((worker) => (
-                      texto === dataName.toLowerCase().substring(0, longNameWorker)
-                    ));
-                    return filterName; // retornamos el array de objetos encontrados
-                  };
+      btnEditWorker.forEach(edit => {
+        edit.addEventListener('click', (e) => {
+          console.log('clickeando editar');
+          console.log(e.target.dataset.id);
+          console.log(doc.data());
+          fs.collection('pages').get().then((querySnapshot) => {
+            querySnapshot.forEach(doc => {
+              console.log(doc.id)
+              if (e.target.dataset.id === doc.id) {
+                console.log(e.target.dataset.id);
+                console.log(doc.id);
+                editableWorker.contentEditable = 'true';
+                editableWorker.forEach(row => {
+                  console.log('holay')
+                  row.addEventListener('click', (e) => {
+                    console.log('holassss')
+                    console.log(e.target.dataset.id);
+                    console.log(doc.id);
+                    if (e.target.dataset.id === doc.id) {
+                      row.contentEditable = 'true';
+                      row.focus() = 'true';
+                    }
+                  })
+                })
 
-                  const searchName = document.querySelector('.searchName');
-                  searchName.addEventListener('input', (evt) => {
-                    const texto = evt.target.value.toLowerCase();// extraemos el valor de la caja de texto
-                   console.log(texto)
-                    const filtroNameWorker = filterPageWorker(page, texto); 
-                  });
+              }
+            })
+          })
+
+        });
+      });
+
+      btnUpdateWorker.forEach(update => {
+        console.log(btnUpdateWorker)
+        update.addEventListener('click', (e) => {
+          console.log(update);
+          const docId = e.target.dataset.id;
+          editableWorker.forEach(row => {
+            console.log('holay')
+            row.addEventListener('click', (e) => {
+              if (e.target.dataset.id === docId) {
+                console.log('aaaaaaaaaa')
+                const nameWorker = row.querySelector('.nameWorkerPage').innerHTML;
+                const month = row.querySelector('.monthPage').innerHTML;
+                const totalPage = row.querySelector('.totalPage').innerHTML;
+                console.log(nameWorker, month, totalPage)
+                const cityRef = firebase.firestore().collection('pages').doc(docId);
+                const res = cityRef.update({
+                  nameWorker,
+                  month,
+                }, {merge: true});
+                alert('Se han actualizado los datos')
+              }
+
+            })
+          })
+
+        })
+      })
+
+
+
+
+
+
+
+
+
+
+      const filterPageWorker = (data, texto) => {
+        const longNameWorker = texto.length;
+        // const dataName =  fs.collection('users').get().then((snapshot) => {console.log(snapshot.docs);
+        //     // setupPosts(snapshot.docs)
+        // });
+        const dataName = page.nameWorker;
+        const filterName = dataName.filter((worker) => (
+          texto === dataName.toLowerCase().substring(0, longNameWorker)
+        ));
+        return filterName; // retornamos el array de objetos encontrados
+      };
+
+      const searchName = document.querySelector('.searchName');
+      searchName.addEventListener('input', (evt) => {
+        const texto = evt.target.value.toLowerCase(); // extraemos el valor de la caja de texto
+        console.log(texto)
+        const filtroNameWorker = filterPageWorker(page, texto);
+      });
     });
   })
- 
+
 })
 
 
@@ -194,6 +234,3 @@ window.addEventListener('DOMContentLoaded', async(e) => {
 //       setupPosts([])
 //     }
 // });
-
-
-
