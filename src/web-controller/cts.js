eb4b2cde66c86abce04bb15e38313e65851a5756
LourdesMp <lourdes.merino@imax.com.pe>
2021-02-17 19:30:21 -0500

    agegando  buscador

--- src/web-controller/cts.js
+++ src/web-controller/cts.js
@@ -1,6 +1,5 @@
 // para mostrar los datos en la tabla'pages'
 const onGetCts = (callback) => fs.collection('cts').onSnapshot(callback);
-
 const getUsers = () => fs.collection('users').get();
 const getCts = () => fs.collection().get();
 
@@ -16,16 +15,47 @@ window.addEventListener('DOMContentLoaded', async(e) => {
       console.log(userLogueado.uid)
       if (cts.idWorkerCts === userLogueado.uid) {
         ctsContainer.innerHTML +=  `
-        <tr>
-        <td>${cts.monthCts}</td>
+        <tr class ="monthCts">
+        <td>${cts.monthCts} - ${cts.year}</td>
         <td>${cts.pageCts}</td>
         <td><a href=${cts.urlBoleta} download="Boleta.pdf"><button><i class="fas fa-download"></i> Descargar</button></a></td>
         <td>${cts.confirmacion === true ? `<input type="checkbox" class="conformidad" value= ${cts.confirmacion}  name="conformidad" data-id="${cts.id}" disabled="disabled"  checked >` : 
         `<input type="checkbox" class="conformidad" value= ${cts.confirmacion}  name="conformidad"  data-id="${cts.id}" > `} </td>
       </tr>`;
+
+      const searchMonth = document.querySelector('.search')
+      console.log(searchMonth);
+
+      const d = document;
+
+      const txt = searchMonth.value;
+      const texto = txt.toLowerCase()
+
+      const monthText = cts.monthCts
+      const salida = monthText.toLowerCase()
+
+      function searchFilter(input, selector) {
+        d.addEventListener('keyup', (e) => {
+          if (e.target.matches(input)) {
+            d.querySelectorAll(selector).forEach((el) =>
+              el.textContent.toLowerCase().includes(e.target.value) ?
+              el.classList.remove("hide") :
+              el.classList.add("hide"))
+          }
+        })
+      }
+
+      searchFilter('.search', '.monthCts', )
+
       }
     
     });
+
+
+
+
+
+
 
 
     const checkboxs = document.querySelectorAll('.conformidad');
@@ -40,7 +70,7 @@ window.addEventListener('DOMContentLoaded', async(e) => {
             if (e.target.dataset.id === doc.id) {
 
               console.log('joalaaa')
-              const r = confirm('¿Desea dar conformidad a su Constancia CTS?')
+              alert('¿Desea dar conformidad a su Constancia CTS?')
               const cityRef = fs.collection('cts').doc(doc.id);
               const res = cityRef.update({
                 confirmacion: true
