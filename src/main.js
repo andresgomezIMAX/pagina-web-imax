cb713df70997fd0fc1ff855e753b7ab12cd03658
LourdesMp <lourdes.merino@imax.com.pe>
2021-03-06 07:54:55 -0500

    firebase en main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import { changeView } from './views-controler/router.js';
 
 const init = () =>{
+    const firebaseConfig = {
+        apiKey: "AIzaSyAPvuYSkfkOfaRObLXvtDiZjVoQGYDIacc",
+        authDomain: "web-imax.firebaseapp.com",
+        projectId: "web-imax",
+        storageBucket: "web-imax.appspot.com",
+        messagingSenderId: "114819498377",
+        appId: "1:114819498377:web:02e7671a7255f711cf6158",
+        measurementId: "G-P8W33C2X62"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
     changeView(window.location.hash)
     window.addEventListener('hashchange', ()=> changeView(window.location.hash))
 
-}
+};
+window.addEventListener('load', init);
 
 
 
