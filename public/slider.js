83a6107480b7348795e9f43f0b495ae62076864a
LourdesMp <lourdes.merino@imax.com.pe>
2021-03-23 17:39:25 -0500

    seccion ultimos proyectos

--- public/slider.js
+++ public/slider.js
@@ -4,3 +4,19 @@ $(document).ready(function(){
         nextText: ""
     });
 })
+
+
+
+
+const simpleCarousel = document.querySelector('.carousel-lista');
+console.log(simpleCarousel);
+new Glider(simpleCarousel, {
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: '.carousel-indicadores',
+    arrows: {
+        prev: '.anterior-carousel',
+        next: '.siguiente-carousel'
+    }
+})
