1ffc6a1fece0806e2e8111a78d55b70eae4aa0c4
LourdesMp <lourdes.merino@imax.com.pe>
2021-04-22 01:22:16 -0500

    funcion para enviar correos en la vista contactanos principal

--- public/index.js
+++ public/index.js
@@ -1,15 +1,34 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+const $form = document.querySelector(".form-contact-single");
+console.log($form);
 
-app.use(express.urlencoded({extended: false}));
+$form.addEventListener("submit", sendEmail);
 
-app.use(express.json());
+function sendEmail(e) {
+  e.preventDefault();
+  const name = document.querySelector(".name").value;
+  const company = document.querySelector(".company").value;
+  const phone = document.querySelector(".phone").value;
+  const email = document.querySelector(".email").value;
+  const message = document.querySelector(".message").value;
 
-app.use(require("./routes/index.js"));
 
-app.use(express.static(path.join(__dirname)));
+  var templateParams = {
+    name,
+    company,
+    phone,
+    email,
+    message
+  };
+  emailjs.send("gmailMessage","template_b5amrsu",templateParams,"user_0loQaVMyGMBSx2TKKJJQW")
+  .then(
+    function (response) {
+      console.log("SUCCESS!", response.status, response.text);
+      $form.reset();
+      document.querySelector("#success").innerHTML = "Email enviado con éxito.";
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
 
-app.listen(3000, () => {
-  console.log("server on port 3000");
-});
+};
