d1ee5c43af4fa4c8ed54b0fcc6c214ef9e24778b
LourdesMp <lourdes.merino@imax.com.pe>
2021-04-23 10:42:21 -0500

    creando collecciones Customers en firebase

--- public/index.js
+++ public/index.js
@@ -1,3 +1,13 @@
+const saveCustomers = (name, company, phone, email, message) => {
+  return fs.collection("customers").add({
+    name,
+    company,
+    phone,
+    email,
+    message,
+  });
+};
+
 const $form = document.querySelector(".form-contact-single");
 console.log($form);
 
@@ -10,25 +20,38 @@ function sendEmail(e) {
   const phone = document.querySelector(".phone").value;
   const email = document.querySelector(".email").value;
   const message = document.querySelector(".message").value;
-
+ 
 
   var templateParams = {
     name,
     company,
     phone,
     email,
-    message
+    message,
   };
-  emailjs.send("gmailMessage","template_b5amrsu",templateParams,"user_0loQaVMyGMBSx2TKKJJQW")
-  .then(
-    function (response) {
-      console.log("SUCCESS!", response.status, response.text);
-      $form.reset();
-      document.querySelector("#success").innerHTML = "Email enviado con éxito.";
-    },
-    function (error) {
-      console.log("FAILED...", error);
-    }
-  );
-
-};
+  emailjs
+    .send(
+      "gmailMessage",
+      "template_b5amrsu",
+      templateParams,
+      "user_0loQaVMyGMBSx2TKKJJQW"
+    )
+    .then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        if (response.status === 200) {
+           saveCustomers(name, company, phone, email, message).then(
+             () => {
+                $form.reset();
+                document.querySelector("#success").innerHTML =
+                  "Email enviado con éxito.";
+             }
+           );
+        }
+       
+      },
+      function (error) {
+        console.log("FAILED...", error);
+      }
+    );
+}
