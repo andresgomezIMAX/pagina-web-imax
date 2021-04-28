4f8f4aca649b1bb2238bb557c1c27fc70006b152
LourdesMp <lourdes.merino@imax.com.pe>
2021-04-28 15:31:38 -0500

    editando funcionalidad de click en whatsApp

--- public/script2.js
+++ public/script2.js
@@ -18,13 +18,13 @@ popupWhatsApp = () => {
   let msg = document.getElementById('whats-in').value;
   let relmsg = msg.replace(/ /g,"%20");
      
-   window.open('https://wa.me/+51999120425?text='+relmsg, '_blank'); 
+   window.open("https://web.whatsapp.com/send?phone=51999120425&text=" +relmsg,"_blank"); 
   
   });
 
-  setTimeout(() => {
-    popup.classList.toggle('is-active-whatsapp-popup');
-  }, 3000);
+  // setTimeout(() => {
+  //   popup.classList.toggle('is-active-whatsapp-popup');
+  // }, 3000);
 }
 
 popupWhatsApp();
